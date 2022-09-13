537c108142bc286dacbcd095d21519040361a716
sofia lopez <sofi.lopez123@outlook.com.ar>
2022-09-12 21:41:27 -0300

    desafiocomplementario

--- algoritmo-desafio-complementarip/coder-desafio-simulador/assets/js/miarchivo.js
+++ algoritmo-desafio-complementarip/coder-desafio-simulador/assets/js/miarchivo.js
@@ -1,53 +1,36 @@
-//productos
-let productos = [
-  { id: 1, nombre: "1-colgante", precio: 1000 },
-  { id: 2, nombre: "2-AritoA", precio: 500 },
-  { id: 3, nombre: "3-AritoB", precio: 500 },
-  { id: 4, nombre: "4-AritoC", precio: 500 },
-  { id: 5, nombre: "5-Jarra", precio: 1200 },
-  { id: 6, nombre: "6-Mate", precio: 800 },
-];
 
-let carrito = [];
-let acceder = prompt(
-  "****Tienda Artesanal Mambo Mistico****\n Desea hacer una compra?"
-);
-while (acceder != "si" && acceder != "no") {
-  alert("Por favor ingresar si o no");
-  acceder = prompt("Desea comprar algo si/no");
-}
-if (acceder == "si") {
-  //mostrar precios de los productos
-  let todoslosProductos = productos.map(
-    (productos) => productos.nombre + " " + productos.precio + "$"
-  );
-  alert("Nuestros productos\n" + todoslosProductos.join(" - "));
-} else if ((acceder = "no")) {
-  alert("Gracias por visitar la pagina,nos vemos pronto");
-}
-console.log(acceder);
+const mostrarProductos = (productos) => {
+  const contenedorProductos = document.getElementById("producto-contenedor")
 
-/*agregar carrito */
+  productos.forEach(producto => {
+      const div = document.createElement("div")
+      div.classList.add("card")
+      div.innerHTML += `<div class="col-md-4">
+                        <div class="card bg-dark ">
+                          <img src="${producto.img}" class="card-img-top" alt="...">
+                          <div class="card-body">
+                              <h2 class="card-title text-white">${producto.nombre}</h2>
+                              <div>
+                              <p class="card-title text-white">Descripción:  ${producto.desc}</p>
+                              <p class="card-text text-white">Precio:$ ${producto.precio}</p>
+                              </div>
+                              <a href="#" class="btn btn-primary btnColgante"id=boton${producto.id}><img src="assets/img/carrito.png" alt="" class="carrito"/>Comprar</a>
 
-function agregarCarrito(productos) {
-  let inicio = 0;
-  let codigo = parseInt(
-    prompt(
-      "****Tienda Artesanal Mambo Mistico****\n  Ingrese el codigo del producto que desea obtener(1-6).\n Para finalizar la compra ingrese el numero 7"
-    )
-  );
-  //pedir codigo del producto
-  while (isNaN(codigo) || codigo != 7) {
-    inicio += productos[codigo - 1]["precio"];
-    codigo = parseInt(
-      prompt(
-        "****Tienda Artesanal Mambo Mistico****\n  Ingrese el codigo del producto que desea obtener(1-6).\n Para finalizar la compra ingrese el numero 7"
-      )
-    );
-  }
-  alert("El total de la compra es: $ " + inicio + "\nGracias por su compra");
+                          </div>
+                          </div>
+                      </div>`
+
+      contenedorProductos.appendChild(div)
+
+      const boton = document.getElementById( `boton${producto.id}` )
+
+      boton.addEventListener('click', ()=> {
+          carritoIndex(producto.id)
+          alert(`Se agrego el producto ${producto.nombre}`)
+      })
+
+  })
 }
 
-//fin de la funcion*/
 
-agregarCarrito(productos);
+mostrarProductos(productos)
