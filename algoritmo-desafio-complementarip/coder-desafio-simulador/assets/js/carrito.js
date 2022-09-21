3c7a45d4e0ec72dfdaf30b657fe3eb4f812603a8
sofia lopez <sofi.lopez123@outlook.com.ar>
2022-09-21 12:16:58 -0300

    actualizado

--- algoritmo-desafio-complementarip/coder-desafio-simulador/assets/js/carrito.js
+++ algoritmo-desafio-complementarip/coder-desafio-simulador/assets/js/carrito.js
@@ -30,6 +30,24 @@ const carritoIndex = (productoId)=>{
                         <button id="eliminar${producto.id}" class="boton-eliminar" ><img src="./assets/img/basura.png" class="basura"/></button>`;
         contenedorCarrito.appendChild(div);
         localStorage.setItem('carritoDeCompras',JSON.stringify(carritoDeCompras))
+
+        const existe = carritoDeCompras.some (producto => producto.id === productoId) 
+
+    if (existe){
+        const producto = carritoDeCompras.map (prod => {
+
+            if (producto.id === productoId){
+                producto.cantidad++
+            }
+        })
+    } else {
+        const item = productos.find((producto) => producto.id === productoId)
+
+        carrito.push(item)
+    }
+ 
+    actualizarCarrito() 
+    
 //borrar elemento por boton basura
         const borrarElemento = document.getElementById("eliminar"+producto.id);
         borrarElemento.addEventListener('click', ()=>{
