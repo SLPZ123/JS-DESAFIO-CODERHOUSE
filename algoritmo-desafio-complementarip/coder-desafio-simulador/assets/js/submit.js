4561a082069c2030d8ca1dd309ccb0058a4739c3
sofia lopez <sofi.lopez123@outlook.com.ar>
2022-10-23 22:42:24 -0300

    commit

--- algoritmo-desafio-complementarip/coder-desafio-simulador/assets/js/submit.js
+++ algoritmo-desafio-complementarip/coder-desafio-simulador/assets/js/submit.js
@@ -1,18 +1,21 @@
-const $form =document.querySelector('#form')
-$form.addEventListener('submit',handleSubmit)
+const form = document.getElementById("form");
+form.addEventListener("submit", handleSubmit);
 
-async function handleSubmit(event){
-    event.preventDefault()
-    const form =  new FormData(this)
-    const response = await  fetch(this.action,{
-        method:this.method,
-        body:form,
-        header:{
-        'Accept':'application/json'
-        }
-            })
-        if(response.ok){
-        this.reset()
-        alert('Formulario enviado')
-        }
-        }
+async function handleSubmit(event) {
+  event.preventDefault();
+  const myForm = new FormData(this);
+  const response = await fetch(this.action, {
+    method: this.method,
+    body: myForm,
+    headers: {
+      'Accept': 'application/json',
+    }
+  });
+  if (response.ok) {
+    this.reset();Swal.fire({
+        title: "Excelente",
+        text: "El formulario fue enviado con exito",
+        icon: "success",
+      });
+  }
+}
