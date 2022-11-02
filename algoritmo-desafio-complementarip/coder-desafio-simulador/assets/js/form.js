16e47a487ffcf7f0b4c10f326dcc4ddcebbdad7e
Sofi <69180287+SLPZ123@users.noreply.github.com>
2022-11-02 12:28:41 -0300

    Update form.js

--- algoritmo-desafio-complementarip/coder-desafio-simulador/assets/js/form.js
+++ algoritmo-desafio-complementarip/coder-desafio-simulador/assets/js/form.js
@@ -1,3 +1,4 @@
+const theForm = document.getElementById("form");
 $(document).ready(function () {
   $("#form").validate({
     //reglas que debe cumplir el formulario
@@ -5,58 +6,65 @@ $(document).ready(function () {
       name: { required: true, minlength: 4 },
       surname: { required: true, minlength: 4 },
       email: { required: true, minlength: 4 },
-      phone: { required: true, number: true, maxlength: 8, minlength: 8 }
+      phone: { required: true, number: true, maxlength: 11, minlength: 8 },
     },
     //mensajes que aparecen
     messages: {
       name: {
         required: "El nombre es requerido",
-        minlength: "Minimo de 4 letras"
+        minlength: "Minimo de 4 letras",
       },
 
       surname: {
         required: "El apellido es requerido",
-        minlength: "Minimo de 4 letras"
+        minlength: "Minimo de 4 letras",
       },
 
       email: {
         required: "El email es requerido",
-        email: "El correo no es valido"
+        email: "El correo no es valido",
       },
 
       phone: {
         required: "El telefono es requerido",
         number: "Solo se aceptan numeros",
         minlength: "Minimo de 8 caracteres",
-        maxlength: "Maximo de 11 caracteres"
-      }
-    }
-  })
-  $("#form").on('submit', function(e){
-		var isvalid = $("#form").valid();
-		if(isvalid){
-			handleSubmit(e);
-		}
-	})
+        maxlength: "Maximo de 11 caracteres",
+      },
+    },
+  });
 });
-const form = document.getElementById('form');
+
+$("#form").on("submit", function (e) {
+  var isvalid = $("#form").valid();
+  if (isvalid) {
+    handleSubmit(e);
+  }
+});
+
+function getData(form) {
+  return new Promise((resolve, reject) => {
+    resolve(
+      fetch(form.action, {
+        method: form.method,
+        body: new FormData(form),
+        headers: {
+          Accept: "application/json",
+        },
+      })
+    );
+  });
+}
 
 async function handleSubmit(event) {
-	event.preventDefault();
-	const myForm = new FormData(form);
-	const response = await fetch(myForm.action, {
-	  method: 'POST',
-	  body: myForm,
-	  headers: {
-		'Accept': 'application/json'
-	  }
-	});
-	if (response.ok) {
-		myForm.reset();
-		Swal.fire({
-		  title: "Excelente",
-		  text: "El formulario fue enviado con exito",
-		  icon: "success"
-		});
-	}
-  }
+  event.preventDefault();
+  const response = await getData(theForm);
+  if (response.ok) {
+    theForm.reset();
+    Swal.fire({
+      title: "Excelente",
+      text: "El formulario fue enviado con exito",
+      icon: "success",
+    });
+  }
+}
